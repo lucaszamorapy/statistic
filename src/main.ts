20b040731a342454075f3e7620e807cf4f2aa7fd
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2024-05-30 00:02:16 -0300

    feat: create table with data

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,40 @@ async function handleData() {
   const data = await fetchData<TransacaoAPI[]>("https://api.origamid.dev/json/transacoes.json?")
   if (!data) return
   const transacao = data.map(normalTransition)
-  console.log(transacao)
+  preencherTabela(transacao)
+}
+
+function preencherTabela(transacoes: Transacao[]): void {
+  const tabela = document.querySelector("#transacoes")
+  if (!tabela) return
+  transacoes.map((item) => (
+    tabela.innerHTML += `<tr>
+      <td>${item.nome}</td>
+      <td>${item.email}</td>
+      <td>R$ ${item.moeda}</td>
+      <td>${item.pagamento}</td>
+      <td>${item.status}</td>
+    </tr>`
+  ))
 }
 
 
 handleData()
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div id="teste">
-   
-  </div>
+  <h2>Dados</h2>
+   <table id="transacoes">
+    <thead>
+      <tr>
+        <th>Nome</th>
+        <th>Email</th>
+        <th>Compra</th>
+        <th>Pagamento</th>
+        <th>Status</th>
+      </tr>
+    </thead>
+   </table>
+  
 `
 
 
